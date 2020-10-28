8c5e60c758157825604efe982453c2212bc97078
Muhidin <muhidinhukic1@gmail.com>
2020-10-27 22:40:25 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+
+    attr_accessor :school_name, :name, :grade
+
+    def initialize(school_name, hash={}, *grade)
+        @school_name = school_name
+        @name = name
+        @grade = grade
+        @hash = hash
+    end
+
+
+    def roster
+        @hash
+    end
+
+
+    def add_student(name, grade)
+        if roster.length == 0 || !@hash.key?(grade)
+            @hash[grade] = [name]
+        elsif roster.length > 0 && @hash.key?(grade)
+            @hash[grade] << name
+        end
+    end
+
+
+    def grade(grade)
+      @hash[grade]
+    end
+    
+    def sort
+        @hash[9] = @hash[9].sort
+        @hash.sort.to_h
+    end
+
+end
